d714e324dd9a88828a4964e03c3ad43b1c16316b
stould <vinirresko@gmail.com>
2018-03-28 03:55:33 +0000

    Added stack, queue on linked list

---
+++ Queue.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node{
+    int val;
+    Node *next;
+};
+
+struct Queue{
+    Node *head;
+    int size;
+    Queue(){
+        head = NULL;
+        size = 0;
+    }
+
+    Node* makeNode(int val){
+        Node *newNode = (Node *) malloc(sizeof(Node));
+        newNode->val = val;
+        newNode->next = NULL;
+        return newNode;
+    }
+
+    int getFront(){
+        if(head == NULL){
+            printf("Erro - queue is NULL\n");
+            return -1;
+        }else{
+            return head->val;
+        }
+    }
+
+    void push(int val){
+        Node *newNode = makeNode(val);
+        if(head == NULL){
+            head = newNode;
+        }else{
+            Node* tmp = head, *last = head;
+            while(tmp != NULL){
+                last = tmp;
+                tmp = tmp->next;
+            }
+            last->next = newNode;
+        }
+        size++;
+    }
+
+    void pop(){
+        if(head == NULL){
+            printf("Erro - Pilha vazia\n");
+        }else{
+            Node *tmp = head;
+            head = head->next;
+            free(tmp);
+        }
+    }
+
+    int getSize(){
+        return size;
+    }
+};
+
+int main(){
+    Queue st;
+    st.push(10);
+    printf("%d\n", st.getFront());
+    st.push(5);
+    printf("%d\n", st.getFront());
+    st.pop();
+    st.push(15);
+    printf("%d\n", st.getFront());
+    st.push(7);
+    printf("%d\n", st.getFront());
+    st.pop();
+    printf("%d\n", st.getFront());
+    st.pop();
+    printf("%d\n", st.getFront());
+    st.pop();
+    printf("%d\n", st.getFront());
+    return 0;
+}
---
+++ Stack.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node{
+    int val;
+    Node *next;
+};
+
+struct Stack{
+    Node *head;
+    int size;
+    Stack(){
+        head = NULL;
+        size = 0;
+    }
+
+    Node* makeNode(int val){
+        Node *newNode = (Node *) malloc(sizeof(Node));
+        newNode->val = val;
+        newNode->next = NULL;
+        return newNode;
+    }
+
+    int getTop(){
+        if(head == NULL){
+            printf("Erro - stack is NULL\n");
+            return -1;
+        }else{
+            return head->val;
+        }
+    }
+
+    void push(int val){
+        Node *newNode = makeNode(val);
+        if(head == NULL){
+            head = newNode;
+        }else{
+            newNode->next = head;
+            head = newNode;
+        }
+        size++;
+    }
+
+    void pop(){
+        if(head == NULL){
+            printf("Erro - Pilha vazia\n");
+        }else{
+            Node *tmp = head;
+            head = head->next;
+            free(tmp);
+        }
+    }
+
+    int getSize(){
+        return size;
+    }
+};
+
+int main(){
+    Stack st;
+    st.push(1);
+    printf("%d\n", st.getTop());
+    st.push(2);
+    printf("%d\n", st.getTop());
+    st.push(3);
+    printf("%d\n", st.getTop());
+    st.pop();
+    printf("%d\n", st.getTop());
+    st.push(4);
+    printf("%d\n", st.getTop());
+    st.push(5);
+    printf("%d\n", st.getTop());
+    st.pop();
+    printf("%d\n", st.getTop());
+    st.pop();
+    printf("%d\n", st.getTop());
+    st.pop();
+    printf("%d\n", st.getTop());
+    st.pop();
+    st.push(7);
+    printf("%d\n", st.getTop());
+    st.push(4);
+    printf("%d\n", st.getTop());
+    st.pop();
+    printf("%d\n", st.getTop());
+    st.pop();
+    printf("%d\n", st.getTop());
+    return 0;
+}
